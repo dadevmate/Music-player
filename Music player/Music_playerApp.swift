1f98d5ca1564f291683debceba2205529bb409e6
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-02-17 13:08:01 +0800

    Initial Commit

---
+++ Music player/Music_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_playerApp.swift
+//  Music player
+//
+//  Created by Nethan on 17/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
